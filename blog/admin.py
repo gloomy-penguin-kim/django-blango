4575fa585d0d756568b88adff0b12f4788193fdd
gloomy-penguin-kim <gloomy.penguin.kim@gmail.com>
2024-12-10 01:08:22 -0600

    Finished Generic Relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 
 admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ('title', 'slug', 'author', 'published_at')
 
 
 admin.site.register(Post, PostAdmin)
 
 
+ 
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'content_object_str', 'content_type', 'creator')
+
+    def content_object_str(self, obj):
+        if obj.content_object:
+            return str(obj.content_object)
+        return '-'
+    
+admin.site.register(Comment, CommentAdmin)
