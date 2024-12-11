b70bcca38820e5ed298fbdb02694df7115a2a03a
gloomy-penguin-kim <gloomy.penguin.kim@gmail.com>
2024-12-11 14:49:40 -0600

    Finished custom template tags

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from django.utils import timezone
-from blog.models import Post
-# def index(request):
-#     return render(request, "blog/index.html")
+from blog.models import Post 
+import logging 
+logger = logging.getLogger(__name__)
 
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     return render(request, "blog/index.html", {"posts": posts})
+
+ 
+def post_detail(request, slug):  
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post": post})
